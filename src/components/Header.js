890ba3e801d420c99d4c54a633ae7f2ecf18604d
alpacacino <alpacacino@yandex.ru>
2017-10-24 17:58:05 +0300

    added nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => (
 	<div className="App-header">
     	<img src={logo} className="App-logo" alt="logo" />
     	<h1 className="App-title">Welcome to React</h1>
+    	<ul>
+	        <li><Link to="/">Home</Link></li>
+	        <li><Link to="/about">About</Link></li>
+	        <li><Link to="/production">Topics</Link></li>
+      	</ul>
     </div>
 );
 
