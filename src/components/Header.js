e446079796b166055729f865bb264b5654fae9c0
alpacacino <alpacacino@yandex.ru>
2017-10-25 17:35:09 +0300

    modified nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom';
-
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => (
-	<div className="App-header">
-    	<img src={logo} className="App-logo" alt="logo" />
-    	<h1 className="App-title">Welcome to React</h1>
-    	<ul>
-	        <li><Link to="/">Home</Link></li>
-	        <li><Link to="/about">About</Link></li>
-	        <li><Link to="/production">Topics</Link></li>
-      	</ul>
-    </div>
+	<nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+		<Link exact className="navbar-brand" to="/"> 
+			<img src={logo} className="App-logo" alt="logo" />
+		</Link>
+		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+		</button>
+		<div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+			<div className="navbar-nav">
+				<NavLink exact activeClassName="active" className="nav-item nav-link" to="/">Home</NavLink>
+				<NavLink activeClassName="active" className="nav-item nav-link" to="/about">About</NavLink>
+				<NavLink activeClassName="active" className="nav-item nav-link" to="/production">Production</NavLink>
+			</div>
+		</div>
+	</nav>
 );
 
 export default Header;
