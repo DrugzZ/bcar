18c13759191a3e7b985e59eb1e6104f36fc2ade7
alpacacino <alpacacino@yandex.ru>
2017-11-01 17:26:34 +0300

    refactored code in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,31 +3,27 @@ import { Link } from 'react-router-dom';
 import record_2e from '../assets/record_2e.png';
 
 const Home = () => (
-	<div className="home-promo-block d-flex align-items-center text-left">
-		<div className="container">
-			<div className="row">
-				<div className="col-md-4 d-flex align-items-center">
-					<div className="promo-block-info">
-					<div className="line">
-						<p className="h4">
-							Представительство
-						</p>
-					</div>
-					<h1 className="font-weight-bold mb-4">
-						Балканкар РЕКОРД
-					</h1>
-					<p className="text-nowrap lead mb-4">
-					Новая линейка экологичных автопогрузчиков RECORD 2E. 
-					<br />Погрузчики данной серии воплощают в себе ряд новейших
-					<br /> достижений компании Балканкар РЕКОРД. 
+	<div className="row d-flex justify-content-center">
+		<div className="col-sm-4 d-flex align-items-center">
+			<div className="home-promo-block-cont">
+				<div className="line">
+					<p className="h4">
+						Представительство
 					</p>
-					<Link to="/production" className="btn btn-danger">Подробнее</Link>
-					</div>
 				</div>
-				<div className="col-md-8">
-					<img src={record_2e} className="img-fluid" alt="Автопогрузчик Balkancar Record 2E" />
-				</div>
+				<h1 className="font-weight-bold mb-4 text-nowrap">
+					Балканкар РЕКОРД
+				</h1>
+				<p className="mb-4 home-promo-block-info">
+				Новая линейка экологичных автопогрузчиков RECORD 2E. 
+				Погрузчики данной серии воплощают в себе ряд новейших
+				достижений компании Балканкар РЕКОРД. 
+				</p>
+				<Link to="/production" className="btn btn-danger">Подробнее</Link>
 			</div>
+		</div>
+		<div className="col-sm-8">
+			<img src={record_2e} className="img-fluid" alt="Автопогрузчик Balkancar Record 2E" />
 		</div>
 	</div>
 );
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import About from './About';
 import Production from './Production';
 
 const Main = () => (
-	<main>
+	<main className="home-promo-block d-flex align-items-center text-left container-fluid">
 	    <Switch>
 			<Route exact path='/' component={Home}/>
 			<Route path='/about' component={About}/>
