cc65d66e2b9bf2e0bd68ec47fabe1e1119c58547
alpacacino <alpacacino@yandex.ru>
2017-11-02 16:12:40 +0300

    added some thigs to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,30 +3,40 @@ import { Link } from 'react-router-dom';
 import record_2e from '../assets/record_2e.png';
 
 const Home = () => (
-	<div className="home-promo-block d-flex align-items-center text-left container-fluid">
-		<div className="row d-flex justify-content-center">
-			<div className="col-sm-4 d-flex align-items-center">
-				<div className="home-promo-block-cont">
-					<div className="line">
-						<p className="h4">
-							Представительство
+	<div className="wrap">
+		<section className="home-promo-block d-flex align-items-center text-left container-fluid">
+			<div className="row d-flex justify-content-center">
+				<div className="col-sm-4 d-flex align-items-center">
+					<div className="home-promo-block-cont ml-md-5">
+						<div className="line">
+							<p className="h4">
+								Представительство
+							</p>
+						</div>
+						<h1 className="font-weight-bold mb-4 text-nowrap">
+							Балканкар РЕКОРД
+						</h1>
+						<p className="mb-4 home-promo-block-info">
+						Новая линейка экологичных автопогрузчиков RECORD 2E. 
+						Погрузчики данной серии воплощают в себе ряд новейших
+						достижений компании Балканкар РЕКОРД. 
 						</p>
+						<Link to="/production" className="btn btn-danger">Подробнее</Link>
 					</div>
-					<h1 className="font-weight-bold mb-4 text-nowrap">
-						Балканкар РЕКОРД
-					</h1>
-					<p className="mb-4 home-promo-block-info">
-					Новая линейка экологичных автопогрузчиков RECORD 2E. 
-					Погрузчики данной серии воплощают в себе ряд новейших
-					достижений компании Балканкар РЕКОРД. 
-					</p>
-					<Link to="/production" className="btn btn-danger">Подробнее</Link>
+				</div>
+				<div className="col-sm-8">
+					<img src={record_2e} className="img-fluid" alt="Автопогрузчик Balkancar Record 2E" />
 				</div>
 			</div>
-			<div className="col-sm-8">
-				<img src={record_2e} className="img-fluid" alt="Автопогрузчик Balkancar Record 2E" />
+		</section>
+		<section className="home-partners-block">
+			<div className="container">
+				<div className="home-partners-block-heading py-5">
+					<h3>Продукция</h3>
+					<p className="text-muted">Мы предлгаеам продукцию следующих производителей</p>
+				</div>
 			</div>
-		</div>
+		</section>
 	</div>
 );
 
