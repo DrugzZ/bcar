a130f8d04f1dbb606a7cdcfc371f1733e1807e4d
alpacacino <alpacacino@yandex.ru>
2017-11-16 15:32:59 +0300

    added working tabs and styled some txt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import record_2e from '../assets/record_2e.png';
+import { src, preview } from '../assets/record_2e.png';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const Home = () => (
 	<div className="wrap">
 		<section className="home-promo-block d-flex align-items-center text-left container-fluid">
 			<div className="row d-flex justify-content-center">
 				<div className="col-sm-4 d-flex align-items-center">
-					<div className="home-promo-block-cont ml-md-5">
+					<div className="home-promo-block-cont">
 						<div className="line">
 							<p className="h4">
 								Представительство
@@ -25,7 +26,7 @@ const Home = () => (
 					</div>
 				</div>
 				<div className="col-sm-8">
-					<img src={record_2e} className="img-fluid" alt="Автопогрузчик Balkancar Record 2E" />
+					<img src={src} className="img-fluid" alt="Автопогрузчик Balkancar Record 2E" />
 				</div>
 			</div>
 		</section>
@@ -35,32 +36,57 @@ const Home = () => (
 					<h3>Продукция</h3>
 					<p className="text-muted">Мы предлгаеам продукцию следующих производителей</p>
 				</div>
-				<div className="row">
-					<div className="col-md-3 home-partners-single">
+				<Tabs selectedTabClassName="home-partners-single--selected">
+				<TabList className="row">
+					<Tab className="col-md-3 home-partners-single">
 						<div className="home-partners-single-img mb-2">
 
 						</div>
 						<p className="home-partners-single-txt">Балканкар РЕКОРД</p>
-					</div>
-					<div className="col-md-3 home-partners-single">
+					</Tab>
+					<Tab className="col-md-3 home-partners-single">
 						<div className="home-partners-single-img mb-2">
 
 						</div>
 						<p className="home-partners-single-txt">Элпром Троян</p>
-					</div>
-					<div className="col-md-3 home-partners-single">
+					</Tab>
+					<Tab className="col-md-3 home-partners-single">
 						<div className="home-partners-single-img mb-2">
 
 						</div>
 						<p className="home-partners-single-txt">EnSol Technology</p>
-					</div>
-					<div className="col-md-3 home-partners-single">
+					</Tab>
+					<Tab className="col-md-3 home-partners-single">
 						<div className="home-partners-single-img mb-2">
 
 						</div>
 						<p className="home-partners-single-txt">Элхим-Искра</p>
-					</div>
+					</Tab>
+				</TabList>
+				<div className="row">
+					<TabPanel className="col-sm-12">
+						<p className="home-partners-single-content">
+						<a href="" className="a--styled">"Балканкар Рекорд" АО</a> ведущий производитель&nbsp;
+						<Link to="/production" className="a--styled">автопогрузчиков</Link>,&nbsp;
+						<Link to="/production" className="a--styled">электропогрузчиков</Link>,&nbsp;
+						<Link to="/production" className="a--styled">электротележек</Link>,&nbsp;
+						<Link to="/production" className="a--styled">тягачей</Link>.
+						Завод находится в г. Пловдив, Болгария. Качество предоставляемых компанией услуг и продукции 
+						подтверждено сертификатами ISO 9001, AQAP 2110 и Сертификатом соответствия по требованиям 
+						таможенного союза России, Беларусь и Казахстан.
+						</p>
+					</TabPanel>
+					<TabPanel className="col-sm-12">
+						<p className="home-partners-single-content">Элпром Троян</p>
+					</TabPanel>
+					<TabPanel className="col-sm-12">
+						<p className="home-partners-single-content">EnSol Technology</p>
+					</TabPanel>
+					<TabPanel className="col-sm-12">
+						<p className="home-partners-single-content">Элхим-Искра</p>
+					</TabPanel>
 				</div>
+				</Tabs>
 			</div>
 		</section>
 	</div>
